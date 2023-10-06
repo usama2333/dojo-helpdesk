6187b79f286bb658b63f5d447547ba4108314bbe
usama2333 <usama.wizz@gmail.com>
2023-10-06 14:38:47 +0500

    first commit

---
+++ app/tickets/[id]/page.jsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import React from 'react'
+
+const getTicket = async (id) => {
+    const res = await fetch(`http://localhost:4000/tickets/${id}`,{
+        next : {
+            revalidate : 60
+        }
+    })
+    return res.json();
+}
+
+const TicketDetails = async ({params}) => {
+    const id = params.id
+ const ticket = await getTicket(id);
+
+  return (
+    <main>
+        <nav>
+            <h2>TicketDetails</h2>
+        </nav>
+        <div className="card">
+           
+            <h3>{ticket.title}</h3>
+            <small>Created by {ticket.user_email}</small>
+            <p>{ticket.body}</p>
+            <div className={`pill ${ticket.priority}`}>
+                {ticket.priority} priority
+            </div>
+          
+        </div>
+    </main>
+  )
+}
+
+export default TicketDetails
